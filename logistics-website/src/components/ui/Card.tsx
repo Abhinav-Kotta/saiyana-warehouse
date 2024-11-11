68e48fb181d7cf0d53d23864571a4c322d1f7257
Abhinav-Kotta <abhinav.kotta@gmail.com>
2024-11-10 19:36:08 -0500

    updated card.tsx file

--- logistics-website/src/components/ui/Card.tsx
+++ logistics-website/src/components/ui/Card.tsx
@@ -1,22 +1,41 @@
+'use client';
+
 import { cn } from "@/lib/utils";
-import { HTMLAttributes, forwardRef } from "react";
+import { forwardRef, HTMLAttributes } from "react";
 
-interface CardProps extends HTMLAttributes<HTMLDivElement> {}
+type CardProps = HTMLAttributes<HTMLDivElement> & {
+  variant?: 'default' | 'outline';
+  size?: 'sm' | 'md' | 'lg';
+};
 
-const Card = forwardRef<HTMLDivElement, CardProps>(
-  ({ className, ...props }, ref) => {
-    return (
-      <div
-        ref={ref}
-        className={cn(
-          "rounded-lg border bg-white p-6 shadow-sm",
-          className
-        )}
-        {...props}
-      />
-    );
-  }
-);
+const Card = forwardRef<HTMLDivElement, CardProps>(({
+  className,
+  variant = 'default',
+  size = 'md',
+  children,
+  ...props
+}, ref) => {
+  return (
+    <div
+      ref={ref}
+      className={cn(
+        "rounded-lg bg-white shadow-sm transition-all",
+        {
+          'border border-gray-200': variant === 'default',
+          'border-2 border-primary-500': variant === 'outline',
+          'p-4': size === 'sm',
+          'p-6': size === 'md',
+          'p-8': size === 'lg',
+        },
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+});
 
 Card.displayName = "Card";
+
 export default Card;
